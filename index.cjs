ed14b2b7300599ae2c9e6c720bce4ff427ee0dd9
Fadli Mukhtarif <fadli.m.m@gmail.com>
2022-04-25 23:06:41 +0700

    add pump-to-agh/mgh, adding conditional each string, move logic conditional pump-pump to conditioal each string, change html option

--- index.cjs
+++ index.cjs
@@ -25,18 +25,52 @@ const wss = new Server({ server });
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     var dataj = JSON.parse(JSON.parse(data));
-    fs.createReadStream('data/extract_Table_connection.csv')
-      .pipe(csv(['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq']))
-      .on('data', (row) => {
-        // console.log(row.ps,dataj.series_upper,':',row.sd,dataj.shft_upper,':',row.ps2,dataj.series_bottom,' ',row.sd2,dataj.shft_bottom);
-        // console.log(row.ps==dataj.series_upper,row.sd==dataj.shft_upper,row.ps2==dataj.series_bottom,row.sd2==dataj.shft_bottom)
-        if(row.ps==dataj.series_upper && row.sd==dataj.shft_upper && row.ps2==dataj.series_bottom && row.sd2==dataj.shft_bottom) {
-          ws.send(row.cpl); 
-        }
-      })
-      .on('end', () => {
-        console.log('CSV file successfully processed');
-      });  
+	// console.log(dataj);
+	if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_pump') {
+		console.log('PUMP to PUMP');
+		fs.createReadStream('data/pump-pump.csv')
+		  .pipe(csv(['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq']))
+		  .on('data', (row) => {
+			// console.log(row.ps,dataj.series_upper,':',row.sd,dataj.shft_upper,':',row.ps2,dataj.series_bottom,' ',row.sd2,dataj.shft_bottom);
+			// console.log(row.ps==dataj.series_upper,row.sd==dataj.shft_upper,row.ps2==dataj.series_bottom,row.sd2==dataj.shft_bottom)
+			if(row.ps==dataj.series_upper && row.sd==dataj.shft_upper && row.ps2==dataj.series_bottom && row.sd2==dataj.shft_bottom) {
+			  ws.send(row.cpl); 
+			}
+		  })
+		  .on('end', () => {
+			// console.log('CSV file successfully processed');
+		  });  
+
+	} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_agh') {
+		console.log('PUMP to AGH');
+	} else if (dataj.esp_upper == 'u_agh' && dataj.esp_bottom == 'b_intake') {
+		console.log('AGH to INTAKE');
+	} else if ((dataj.esp_upper == 'u_pump' || 
+				dataj.esp_upper == 'b_agh') && dataj.esp_bottom == 'b_protector') {
+		console.log('PUMP or AGH/MGH to Protector');
+	} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_intake') {
+		console.log('PUMP to INTAKE');
+	} else if (dataj.esp_upper == 'u_intake' && dataj.esp_bottom == 'b_protector') {
+		console.log('INTAKE to PROTECTOR');
+	} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_protector') {
+		console.log('PROTECTOR to PROTECTOR');
+	} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_motor') {
+		console.log('PROTECTOR to MOTOR');
+	} else if (dataj.esp_upper == 'u_motor' && dataj.esp_bottom == 'b_motor') {
+		console.log('MOTOR to MOTOR');
+	} else if ((dataj.esp_upper == 'u_pump' ||
+				dataj.esp_upper == 'u_agh' ||
+				dataj.esp_upper == 'u_intake') && dataj.esp_bottom == 'b_maxProtector') {
+		console.log('AGH/INTAKE/PUMP to PROTECTOR MAXIMUS');
+	} else if (dataj.esp_upper == 'u_maxProtector' && dataj.esp_bottom == 'b_maxProtector') {
+		console.log('MAXIMUS PROTECTOR to MAXIMUS PROTECTOR');
+	} else if (dataj.esp_upper == 'u_maxProtector' && (dataj.esp_bottom == 'b_motor' || dataj.esp_bottom == 'b_maxMotor')) {
+		console.log('MAXIMUS PROTECTOR to MOTOR/MAXIMUS MOTOR');
+	} else if (dataj.esp_upper == 'u_maxMotor' && dataj.esp_bottom == 'b_maxMotor') {
+		console.log('MAXIMUS MOTOR to MAXIMUS MOTOR');
+	} else {
+		console.log('Not Recognized');
+	}
 
   });
 

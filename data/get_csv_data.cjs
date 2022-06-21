3f1c571765d14ee85db8b6fa4bd901742a2e4d38
Fadli Mukhtarif <fadli.m.m@gmail.com>
2022-06-21 19:51:15 +0700

    add more csv, modified coupling.html, split js variable series-shaft

---
+++ data/get_csv_data.cjs
@@ -0,0 +1,135 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let table_list;
+let datafile;
+
+
+// const cek="pump-pump";
+// const cek="pump-agh_mgh";
+// const cek="aghmgh-intake";
+// const cek="pump_agh-protector";
+// const cek="pump-intake";
+// const cek="intake-protector";
+// const cek="protector-protector";
+// const cek="protector-motor";
+// const cek="motor-motor";
+// const cek="maxProtector-maxProtector_promotor";
+// const cek="maxProtector-std_motor_dominator_max_motor";
+// const cek="maxUT_motor_promotor-maxCT_LT_motor";
+const cek="mgh_agh_intake_pump_LT-Max_prot_promotor";
+
+
+console.log('\n\n**'+cek+'**\n\n');
+if(cek=='pump-pump') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq'];
+	datafile='../data/pump-pump.csv';
+} else if (cek=='pump-agh_mgh') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];	
+	datafile='../data/pump-agh_mgh.csv';
+} else if (cek=='aghmgh-intake') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq'];	
+	datafile='../data/aghmgh-intake.csv';
+} else if(cek=="pump_agh-protector") {
+	table_list=['ps','sd','ps2','sd2','osize','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/pump_agh_mgh-protector.csv';
+} else if (cek=="pump-intake") {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/pump-intake.csv';
+} else if (cek=='intake-protector') {
+	table_list=['ps','sd','ps2','sd2','osize','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/intake-protector.csv';
+} else if (cek=='protector-protector') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/protector-protector.csv';
+} else if (cek=='protector-motor') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','sa','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/protector-motor.csv';
+} else if (cek=='motor-motor') {
+	table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+	datafile='../data/motor-motor.csv';
+} else if (cek=='maxProtector-maxProtector_promotor') {
+	table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','cpl_inc718','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+	datafile='../data/maxProtector-maxProtector_promotor.csv';
+} else if(cek=='maxProtector-std_motor_dominator_max_motor') {
+	table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','cpl_inc718','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+	datafile='../data/maxProtector-std_motor_dominator_max_motor.csv';
+} else if(cek=='maxUT_motor_promotor-maxCT_LT_motor') {
+	table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+	datafile='../data/maxUT_motor_promotor-maxCT_LT_motor.csv';
+} else if(cek=='mgh_agh_intake_pump_LT-Max_prot_promotor') {
+	table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','cpl_inc718','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+	datafile='../data/mgh_agh_intake_pump_LT-Max_prot_promotor.csv';
+}
+let results = [];
+let z = [];
+let flag;
+
+fs.createReadStream(datafile)
+  .pipe(csv(table_list))
+  .on('data', (row) => {
+  	results.push(row);
+  }).on('end', () => {
+  	// console.log(results);
+
+
+  	console.log("===PS===\n\[");
+  	for(let n in results) {
+  		flag=false;
+  		for(let x in z){
+			if(z[x].ps==results[n].ps) flag=true;
+		}
+		if(!flag) {
+			console.log('\t\''+results[n].ps+'\',');
+			z[n]=results[n];
+		}
+  	}
+  	console.log('\]');
+
+  	z=[];
+	console.log("\n===PS2===\n\[");
+	for(let n in results) {
+  		flag=false;
+  		for(let x in z){
+			if(z[x].ps2==results[n].ps2) flag=true;
+		}
+		if(!flag) {
+			console.log('\t\''+results[n].ps2+'\',');
+			z[n]=results[n];
+		}
+  	}
+  	console.log('\]');
+
+  	z=[];
+	console.log("\n===SD===\n\[");
+	for(let n in results) {
+  		flag=false;
+  		for(let x in z){
+			if(z[x].sd==results[n].sd) flag=true;
+		}
+		if(!flag) {
+			console.log('\t\''+results[n].sd+'\',');
+			z[n]=results[n];
+		}
+  	}
+  	console.log('\]');
+
+
+  	z=[];
+	console.log("\n===SD2===\n\[");
+	for(let n in results) {
+  		flag=false;
+  		for(let x in z){
+			if(z[x].sd2==results[n].sd2) flag=true;
+		}
+		if(!flag) {
+			console.log('\t\''+results[n].sd2+'\',');
+			z[n]=results[n];
+		}
+  	}
+  	console.log('\]');
+
+  });  
--- index.cjs
+++ index.cjs
@@ -7,7 +7,7 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/coupling.html';
+const INDEX = '/coupling';
 
 const server = express()
   .use(express.static(path.join(__dirname, 'www')))
@@ -25,53 +25,105 @@ const wss = new Server({ server });
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     var dataj = JSON.parse(JSON.parse(data));
+    var esp_type;
+    var table_list;
 	// console.log(dataj);
-	if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_pump') {
-		console.log('PUMP to PUMP');
-		fs.createReadStream('data/pump-pump.csv')
-		  .pipe(csv(['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq']))
+		if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_pump') {
+			console.log('PUMP to PUMP');
+			esp_type='data/pump-pump.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_agh') {
+			console.log('PUMP to AGH/MGH');
+			esp_type='data/pump-agh_mgh.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_agh' && dataj.esp_bottom == 'b_intake') {
+			console.log('AGH to INTAKE');
+			esp_type='data/aghmgh-intake.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if ((dataj.esp_upper == 'u_pump' || dataj.esp_upper == 'b_agh') && dataj.esp_bottom == 'b_protector') {
+			console.log('PUMP or AGH/MGH to Protector');
+			esp_type='data/pump_agh_mgh-protector.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','cpl','css','cslw','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_intake') {
+			console.log('PUMP to INTAKE');
+			esp_type='data/pump-intake.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_intake' && dataj.esp_bottom == 'b_protector') {
+			console.log('INTAKE to PROTECTOR');
+			esp_type='data/intake-protector.csv';
+			table_list=['ps','sd','ps2','sd2','osize','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_protector') {
+			console.log('PROTECTOR to PROTECTOR');
+			esp_type='data/protector-protector.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_motor') {
+			console.log('PROTECTOR to MOTOR');
+			esp_type='data/protector-motor.csv';
+			table_list=['ps','sd','ps2','sd2','osize','oaflas','ohsn','csfa','cpl','sa','css','cslw','rfa','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_motor' && dataj.esp_bottom == 'b_motor') {
+			console.log('MOTOR to MOTOR');
+			esp_type='data/motor-motor.csv';
+		} else if ((dataj.esp_upper == 'u_pump' ||
+					dataj.esp_upper == 'u_agh' ||
+					dataj.esp_upper == 'u_intake') && dataj.esp_bottom == 'b_maxProtector') {
+			console.log('AGH/INTAKE/PUMP LT to PROTECTOR MAXIMUS'); // This will pass for this momment
+		} else if (dataj.esp_upper == 'u_maxProtector' && dataj.esp_bottom == 'b_maxProtector') {
+			console.log('MAXIMUS PROTECTOR to MAXIMUS PROTECTOR');
+			esp_type='data/maxProtector-maxProtector_promotor.csv';
+			table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','cpl_inc718','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+		} else if (dataj.esp_upper == 'u_maxProtector' && (dataj.esp_bottom == 'b_motor' || dataj.esp_bottom == 'b_maxMotor')) {
+			console.log('MAXIMUS PROTECTOR to MOTOR/MAXIMUS MOTOR');
+			esp_type='data/maxProtector-std_motor_dominator_max_motor.csv'
+		} else if (dataj.esp_upper == 'u_maxMotor' && dataj.esp_bottom == 'b_maxMotor') {
+			console.log('MAXIMUS MOTOR to MAXIMUS MOTOR');
+			esp_type='maxUT_motor_promotor-maxCT_LT_motor.csv'
+			table_list=['sname','ps','sd','sname2','ps2','sd2','cpl','cpl_inc625','sa','csfa','rfa','osize','oaflas','ohsn','css','cslw','ms','mlw','sw','trq'];
+		} else {
+			console.log('Not Recognized');
+		}
+		fs.createReadStream(esp_type)
+		  .pipe(csv(table_list))
 		  .on('data', (row) => {
-			// console.log(row.ps,dataj.series_upper,':',row.sd,dataj.shft_upper,':',row.ps2,dataj.series_bottom,' ',row.sd2,dataj.shft_bottom);
+			// console.log(row.ps,dataj.series_upper,':',row.sd,dataj.shft_upper,':','\"'+row.ps2+'\"','\"'+dataj.series_bottom+'\"',':','\"'+row.sd2+'\"','\"'+dataj.shft_bottom+'\"');
 			// console.log(row.ps==dataj.series_upper,row.sd==dataj.shft_upper,row.ps2==dataj.series_bottom,row.sd2==dataj.shft_bottom)
-			if(row.ps==dataj.series_upper && row.sd==dataj.shft_upper && row.ps2==dataj.series_bottom && row.sd2==dataj.shft_bottom) {
-			  ws.send(row.cpl); 
-			}
+				if(row.ps==dataj.series_upper && row.sd==dataj.shft_upper && row.ps2==dataj.series_bottom && row.sd2==dataj.shft_bottom) {
+				  ws.send(row.cpl); 
+				}
 		  })
 		  .on('end', () => {
-			// console.log('CSV file successfully processed');
+			// console.log();
 		  });  
-
-	} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_agh') {
-		console.log('PUMP to AGH');
-	} else if (dataj.esp_upper == 'u_agh' && dataj.esp_bottom == 'b_intake') {
-		console.log('AGH to INTAKE');
-	} else if ((dataj.esp_upper == 'u_pump' || 
-				dataj.esp_upper == 'b_agh') && dataj.esp_bottom == 'b_protector') {
-		console.log('PUMP or AGH/MGH to Protector');
-	} else if (dataj.esp_upper == 'u_pump' && dataj.esp_bottom == 'b_intake') {
-		console.log('PUMP to INTAKE');
-	} else if (dataj.esp_upper == 'u_intake' && dataj.esp_bottom == 'b_protector') {
-		console.log('INTAKE to PROTECTOR');
-	} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_protector') {
-		console.log('PROTECTOR to PROTECTOR');
-	} else if (dataj.esp_upper == 'u_protector' && dataj.esp_bottom == 'b_motor') {
-		console.log('PROTECTOR to MOTOR');
-	} else if (dataj.esp_upper == 'u_motor' && dataj.esp_bottom == 'b_motor') {
-		console.log('MOTOR to MOTOR');
-	} else if ((dataj.esp_upper == 'u_pump' ||
-				dataj.esp_upper == 'u_agh' ||
-				dataj.esp_upper == 'u_intake') && dataj.esp_bottom == 'b_maxProtector') {
-		console.log('AGH/INTAKE/PUMP to PROTECTOR MAXIMUS');
-	} else if (dataj.esp_upper == 'u_maxProtector' && dataj.esp_bottom == 'b_maxProtector') {
-		console.log('MAXIMUS PROTECTOR to MAXIMUS PROTECTOR');
-	} else if (dataj.esp_upper == 'u_maxProtector' && (dataj.esp_bottom == 'b_motor' || dataj.esp_bottom == 'b_maxMotor')) {
-		console.log('MAXIMUS PROTECTOR to MOTOR/MAXIMUS MOTOR');
-	} else if (dataj.esp_upper == 'u_maxMotor' && dataj.esp_bottom == 'b_maxMotor') {
-		console.log('MAXIMUS MOTOR to MAXIMUS MOTOR');
-	} else {
-		console.log('Not Recognized');
-	}
-
-  });
+	});
 
 });
+
+
+/*************
+Appendix
+dataj = data JSON from Websocket
+row = data from CSV 
+series_upper = 338-1000
+shft_upper = 0.62-1.18
+series_bottom = 338-1000
+shft_bottom = 0.62-1.18
+
+
+ps = string upper
+sd = shaft diameter upper
+ps2 = string bottom
+sd2 = shaft diameter bottom
+osize = oring size
+oaflas = oring aflas p/n
+ohsn = oring hsn p/n
+cpl = coupling monel p/n
+cpl_inc625 = coupling INC 625 p/n
+cpl_inc718 = coupling INC 718 p/n
+csfa = CS Flange Adapter
+css = cs screw
+cslw = cs lockwasher
+rfa = rloy flange adapter
+ms = monel washer
+mlw = monel lockwasher
+sw = wrench size
+trq = torques bolt setting
+**************/
